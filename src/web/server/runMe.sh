2a2462cce4e361f4c1ac545d2360ca8a0a5f81ca
Don Patterson <d_j_p_3@djp3.net>
2015-06-25 17:23:09 -0400

    Updating run script

--- src/web/server/runMe.sh
+++ src/web/server/runMe.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar QualoscopyWebServer -Dfile.encoding=UTF-8 -Duser.timezone=GMT  -Djava.net.preferIPv6Stack=true -Xmx7000M
+java -jar QualoscopyWebServer.jar -Dfile.encoding=UTF-8 -Duser.timezone=GMT  -Djava.net.preferIPv6Stack=true -Xmx7000M
