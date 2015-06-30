d7df96a5a6aaf88bfeedfe19e12262dfb1400dbe
lukeraus <lraus@uci.edu>
2015-06-30 11:02:37 -0700

    Jsonp
    Jsonp Prototype

--- src/web/client/js/login.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#login').click(function() {
-    var email = $('#email').val();
-    var pword = $('#pword').val();
-    console.log("Email=" + email + " Password=" + pword);
-  });
-});
