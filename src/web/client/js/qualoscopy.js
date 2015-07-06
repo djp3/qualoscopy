a2ff2177d45e905595bbb946738e308b277bbd20
lukeraus <lraus@uci.edu>
2015-07-06 14:07:14 -0700

    Cookies and initiate Session

--- src/web/client/js/qualoscopy.js
+++ src/web/client/js/qualoscopy.js
@@ -5,6 +5,42 @@ $(document).ready(function() {
   var port = 9021;
   var versionNumber = 0.1;
 
+  // For managing Cookies
+  function setCookie(name, value, days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  function addToCookieArray(name, data, days) {
+    if(getCookie(name) != null){
+      var cookieArray = JSON.parse(getCookie(name));
+      cookieArray.push(data);
+      setCookie(name, JSON.stringify(cookieArray), days);
+    }
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  function clearCookie(name) {
+    setCookie(name,"",-1);
+  }
+
+
+
   // For the Login
   $('#login').click(function() {
     var email = $('#email').val();
@@ -13,16 +49,48 @@ $(document).ready(function() {
     window.location.href = "main.html";
   });
 
-  $('#jsonp').click(function() {
-    $.ajax({
-      dataType: 'jsonp',
-      url: "http://" + ipAddress + ":" + port + "/version",
-      data: { version: versionNumber},
-      context: document.body
-    }).done(function(data) {
-      console.log(data);
-      document.getElementById("jtext").innerHTML='Hello jsonp! error='
-      + data.error + " version= " + data.version;
-    });
+  $('#add').click(function() {
+    if(getCookie("session_id") != null){
+      $.ajax({
+        dataType: 'jsonp',
+        url: "https://" + ipAddress + ":" + port
+            + "/initiate_session",
+        data: {"version": versionNumber},
+        context: document.body
+      }).done(function(data) {
+        console.log(data);
+        if(data.error == "false"){
+          addToCookieArray("salts", data.session_salt, 1);
+        }
+      });
+    }
+  });
+
+  $('#read').click(function() {
+    console.log(JSON.parse(getCookie("salts")));
+    console.log(getCookie("session_id"));
+  });
+
+
+  $('#create').click(function() {
+    console.log(JSON.parse(getCookie("salts")));
+    console.log(getCookie("session_id"));
+    clearCookie("session_id");
+    clearCookie("salts");
+    if(getCookie("session_id") == null){
+      $.ajax({
+        dataType: 'jsonp',
+        url: "https://" + ipAddress + ":" + port
+            + "/initiate_session",
+        data: {"version": versionNumber},
+        context: document.body
+      }).done(function(data) {
+        console.log(data);
+        if(data.error == "false"){
+          setCookie("session_id", data.session_id, 1);
+          setCookie("salts", JSON.stringify([data.session_salt]), 1);
+        }
+      });
+    }
   });
 });
