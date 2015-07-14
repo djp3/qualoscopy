2aa4149407e6bed658db68d41e84fb25cc874bc3
lukeraus <lraus@uci.edu>
2015-07-14 14:03:42 -0700

    Login!!

--- src/web/client/js/my/qualoscopy.js
+++ src/web/client/js/my/qualoscopy.js
@@ -4,105 +4,112 @@ $(document).ready(function() {
   var ipAddress = "104.237.152.218";
   var port = 9021;
   var versionNumber = 0.1;
+  var debug = true;
 
-  // For managing Cookies
-  function setCookie(name, value, days) {
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days*24*60*60*1000));
-      var expires = "; expires=" + date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name + "=" + value + expires + "; path=/";
-  }
-
-  function addToCookieArray(name, data, days) {
-    if(getCookie(name) != null){
-      var cookieArray = JSON.parse(getCookie(name));
-      cookieArray.push(data);
-      setCookie(name, JSON.stringify(cookieArray), days);
-    }
-  }
-
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
-
-  function clearCookie(name) {
-    setCookie(name,"",-1);
-  }
-
-
-
-  // For the Login
-  $('#login').click(function() {
-    var email = $('#email').val();
-    var pword = $('#pword').val();
-    console.log("Email=" + email + " Password=" + pword);
-    window.location.href = "main.html";
-  });
-
-  $('#add').click(function() {
-    if(getCookie("session_id") != null){
-      $.ajax({
-        dataType: 'jsonp',
-        url: "https://" + ipAddress + ":" + port
-            + "/initiate_session",
-        data: {"version": versionNumber},
-        context: document.body
-      }).done(function(data) {
-        console.log(data);
-        if(data.error == "false"){
-          addToCookieArray("salts", data.session_salt, 1);
-        }
-      });
-    }
-  });
-
-  // $('#read').click(function() {
-  //   console.log(JSON.parse(getCookie("salts")));
-  //   console.log(getCookie("session_id"));
-  // });
-
-  $('#read').click(function() {
+  var initiateSession = function () {
     $.ajax({
       dataType: 'jsonp',
       url: "https://" + ipAddress + ":" + port
-          + "/version",
+      + "/initiate_session",
       data: {"version": versionNumber},
       context: document.body
     }).done(function(data) {
-      console.log(data);
+      if (debug) console.log(data);
+      if(data.error == "false"){
+        Cookies.setCookie("session_id", data.session_id, 1);
+        Cookies.setCookie("salts", JSON.stringify([data.session_salt]), 1);
+        Cookies.setCookie("session_key", "", 1);
+        Cookies.setCookie("user_id", "", 1);
+      }
     });
-  });
+  }
 
+  var checkSession = function(salts, session_id, session_key, user_id){
+    var usedSalt = salts[0];
+    salts.splice(0,1);
+    Cookies.setCookie("salts", JSON.stringify(salts), 1);
+    $.ajax({
+      dataType: 'jsonp',
+      url: "https://" + ipAddress + ":" + port
+      + "/check_session",
+      data: {"user_id": user_id, "shsid": Sha256.hash(session_id + "" + usedSalt),
+      "shsk": Sha256.hash(session_key + "" + usedSalt), "version": versionNumber},
+      context: document.body
+    }).done(function(data) {
+      if (debug) console.log(data);
+      if(data.error == "false" && data.valid == "true"){
+        var setSalts = JSON.parse(Cookies.getCookie("salts"));
+        setSalts.push(data.salt);
+        if (debug) console.log(setSalts);
+        Cookies.setCookie("salts", JSON.stringify(setSalts), 1);
+        window.location.href = "admin.html";
+      } else {
+        initiateSession();
+      }
+    });
+  }
 
-  $('#create').click(function() {
-    console.log(JSON.parse(getCookie("salts")));
-    console.log(getCookie("session_id"));
-    clearCookie("session_id");
-    clearCookie("salts");
-    if(getCookie("session_id") == null){
-      $.ajax({
-        dataType: 'jsonp',
-        url: "https://" + ipAddress + ":" + port
-            + "/initiate_session",
-        data: {"version": versionNumber},
-        context: document.body
-      }).done(function(data) {
-        console.log(data);
-        if(data.error == "false"){
-          setCookie("session_id", data.session_id, 1);
-          setCookie("salts", JSON.stringify([data.session_salt]), 1);
-        }
-      });
+  var login = function(){
+    var user_id = $('#userID').val();
+    var pword = $('#pword').val();
+    var salts = JSON.parse(Cookies.getCookie("salts"));
+    var hashP = Sha256.hash(Sha256.hash(pword) + salts[0]);
+    salts.splice(0,1);
+    Cookies.setCookie("salts", JSON.stringify(salts), 1);
+    Cookies.setCookie("user_id", user_id, 1);
+    if (debug) console.log("Email=" + user_id + " Password=" + pword);
+    $.ajax({
+      dataType: 'jsonp',
+      url: "https://" + ipAddress + ":" + port
+      + "/login",
+      data: {"user_id": user_id, "session_id": Cookies.getCookie("session_id"),
+      shp: hashP,  "version": versionNumber},
+      context: document.body
+    }).done(function(data) {
+      if (debug) console.log(data);
+      if(data.error == "false"){
+        Cookies.setCookie("session_key", data.session_key, 1);
+        var setSalts = JSON.parse(Cookies.getCookie("salts"));
+        setSalts.push(data.salt);
+        if (debug) console.log(setSalts);
+        Cookies.setCookie("salts", JSON.stringify(setSalts), 1);
+        window.location.href = "admin.html";
+      } else {
+        initiateSession();
+      }
+    });
+  }
+
+  // For the Login
+  $('#login-form').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+      login();
     }
   });
-});
+  $('#login').click(function() {
+    login();
+  });
+
+
+
+  // On start up read these fields
+  var salts = JSON.parse(Cookies.getCookie("salts"));
+  var session_id = Cookies.getCookie("session_id");
+  var session_key = Cookies.getCookie("session_key");
+  var user_id = Cookies.getCookie("user_id");
+  if (debug){
+    console.log(salts);
+    console.log("session_id=" + session_id
+    + " session_key=" + session_key + " user_id=" + user_id);
+  }
+
+  if(session_id == null || session_key == null ||
+    salts.length == 0 || salts[0] == null){
+      initiateSession();
+    } else {
+      checkSession(salts, session_id, session_key, user_id);
+    }
+
+
+  });
