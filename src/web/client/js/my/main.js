5a75196c772fc81f944da791ef07537b9dcd5dfd
lukeraus <lraus@uci.edu>
2015-08-05 21:16:09 -0700

    More Refractoring

--- src/web/client/js/my/main.js
+++ src/web/client/js/my/main.js
@@ -300,61 +300,6 @@ $(document).ready(function() {
     populatePolypsTable();
   }
 
-  // Update the tables be deleting them and creating them with new data
-  var updateTables = function(procedure){
-    $("#tables").empty();
-    populateTables(procedure);
-  }
-
-  // Turn on off the elements in an form
-  var toggleForm = function(id, disabled){
-    var form = document.getElementById(id);
-    var elements = form.elements;
-    if (disabled == true){
-      for (var i = 0, len = elements.length; i < len; ++i) {
-        elements[i].setAttribute("disabled", true);
-      }
-    } else {
-      for (var i = 0, len = elements.length; i < len; ++i) {
-        elements[i].removeAttribute("disabled");
-      }
-    }
-  }
-
-  // Toggle the progress bar on and off
-  var toggleProgressBar = function(show){
-    if (show == true){
-      $('.progress').removeClass('hide');
-    } else {
-      $(".progress").addClass('hide');
-    }
-  }
-
-  // Save the procuder form (ops is an javascript object with mutiple keys and values)
-  var saveProcedureForm = function(opts, formID, modalID){
-    toggleProgressBar(true);
-    toggleForm(formID, true);
-
-    var salts = JSON.parse(Cookies.getCookie("salts"));
-    var procedure_id = Cookies.getCookie("procedure_id");
-    var patient_id = Cookies.getCookie("patient_id");
-
-    if (procedure_id != null){
-      updateProcedure(salts, user_id, session_id, session_key, patient_id,
-        procedure_id, opts).done(
-          function(data) {
-            if (debug) console.log(data);
-            if(data.error == "false"){
-              Cookies.addToCookieArray("salts", data.salt, 1);
-              updateTables(data.procedure);
-              toggleForm(formID, false);
-              toggleProgressBar(false);
-              $(modalID).modal('toggle');
-            }
-          });
-      }
-  }
-
   // Create HTML button list
   var buttonListMaker = function(required, valueList, textList, buttonGroupName, height){
     var elementString = "<div class='button-list' style='height:" + height + "'> \
@@ -579,6 +524,61 @@ $(document).ready(function() {
     loadSedationButtonList();
     loadExtentButtonList();
     loadAddPolypSelectOptions();
+  }
+
+  // Update the tables be deleting them and creating them with new data
+  var updateTables = function(procedure){
+    $("#tables").empty();
+    populateTables(procedure);
+  }
+
+  // Turn on off the elements in an form
+  var toggleForm = function(id, disabled){
+    var form = document.getElementById(id);
+    var elements = form.elements;
+    if (disabled == true){
+      for (var i = 0, len = elements.length; i < len; ++i) {
+        elements[i].setAttribute("disabled", true);
+      }
+    } else {
+      for (var i = 0, len = elements.length; i < len; ++i) {
+        elements[i].removeAttribute("disabled");
+      }
+    }
+  }
+
+  // Toggle the progress bar on and off
+  var toggleProgressBar = function(show){
+    if (show == true){
+      $('.progress').removeClass('hide');
+    } else {
+      $(".progress").addClass('hide');
+    }
+  }
+
+  // Save the procuder form (ops is an javascript object with mutiple keys and values)
+  var saveProcedureForm = function(opts, formID, modalID){
+    toggleProgressBar(true);
+    toggleForm(formID, true);
+
+    var salts = JSON.parse(Cookies.getCookie("salts"));
+    var procedure_id = Cookies.getCookie("procedure_id");
+    var patient_id = Cookies.getCookie("patient_id");
+
+    if (procedure_id != null){
+      updateProcedure(salts, user_id, session_id, session_key, patient_id,
+        procedure_id, opts).done(
+          function(data) {
+            if (debug) console.log(data);
+            if(data.error == "false"){
+              Cookies.addToCookieArray("salts", data.salt, 1);
+              updateTables(data.procedure);
+              toggleForm(formID, false);
+              toggleProgressBar(false);
+              $(modalID).modal('toggle');
+            }
+          });
+      }
   }
 
   // Populate the tables on reloads
